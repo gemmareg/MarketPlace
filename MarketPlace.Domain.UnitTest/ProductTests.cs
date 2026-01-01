27398352b99d0512ccde1167a4b2b575732abe80
Adrián <adrian.n.zarate.rivarola@gmail.com>
2026-01-01 20:22:18 +0100

    Added ProductTests
    Right now it only has the structure so later we can add the tests for Products.

---
+++ MarketPlace.Domain.UnitTest/ProductTests.cs
@@ -0,0 +1,20 @@
+﻿using MarketPlace.Shared;
+using static MarketPlace.Shared.Enums;
+
+namespace MarketPlace.Domain.UnitTest
+{
+    public class ProductTests
+    {
+        public ProductTests()
+        {
+            //TODO: finish the constructor with the necessary data
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public void CreateShouldReturnExpectedResult(int dummyVariable)
+        {
+            //TODO: implement the method to run the tests
+        }
+    }
+}
