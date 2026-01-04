08fe9ee79f413474b124061cc48c19042d22ae5a
Gemma <gemmaregues@gmail.com>
2026-01-04 17:59:51 +0100

    Application 1. Added packages on application layer 2. Added UnitOfWork to save changes in handlers. 3. DI of application layer

---
+++ MarketPlace.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MarketPlace.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(cfg => { }, Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            return services;
+        }
+    }
+}
---
+++ MarketPlace.Infrastructure/Persistance/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using MarketPlace.Application.Abstractions.UnitOfWork;
+using MarketPlace.Infrastructure.Persistance.Context;
+
+namespace MarketPlace.Infrastructure.Persistance.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MarketPlaceDbContext _context;
+
+        public UnitOfWork(MarketPlaceDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
